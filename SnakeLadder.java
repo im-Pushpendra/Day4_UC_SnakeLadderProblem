4f1ce2f4f2a8a14403de16bedf0efaf590b142ab
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-09-20 23:50:28 +0530

    uc-3 Player-Noplay ladder snake

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -9,8 +9,23 @@ public class SnakeLadder {
 		System.out.println();
 		int Singleplayer=0;
 		System.out.println("Starting position of Single Player is: "+Singleplayer);
-		int SingleplayerRolls= random.nextInt(6)+1;
-		System.out.println("The Player rolls the die & get a number: "+SingleplayerRolls);
-	}
 
-}
+			int SingleplayerRolls= random.nextInt(6)+1;
+			int a= random.nextInt(3);
+			System.out.println();
+		System.out.println("The Player rolls the die & get a number: "+SingleplayerRolls);
+		if(a==0) {
+		System.out.println("No play - stays in the same psition");
+		}		
+		if(a==1) {	
+			Singleplayer += SingleplayerRolls;
+		System.out.println("Gets Ladder & Player moves ahead by number of psition: "+SingleplayerRolls);
+		System.out.println("Player current position: "+Singleplayer);
+		}
+		if(a==2) {	
+			Singleplayer -= SingleplayerRolls;
+		System.out.println("Snake bytes & Player moves Behind by number of psition: "+"-"+SingleplayerRolls);
+		System.out.println("Player current position: "+Singleplayer);
+		}
+	}
+	}
