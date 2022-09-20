b0366f9e549b42ad792a3bc030dd31275b63e40f
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-09-20 17:26:16 +0530

    Snake & Ladder Simulator

---
+++ SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.day4.UC_SnakeLadderProblem;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Snake & Ladder Simulator");
+	}
+
+}
