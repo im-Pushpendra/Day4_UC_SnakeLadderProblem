551373a4e84447a5f79211cd1da4a5d9661b3643
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-09-20 17:37:26 +0530

    uc-1 Player Starting Position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,6 +4,9 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Snake & Ladder Simulator");
+		System.out.println();
+		int Singleplayer=0;
+		System.out.println("Starting position of Single Player is: "+Singleplayer);
 	}
 
 }
