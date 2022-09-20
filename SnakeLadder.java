3117fe876e462c5c125df7ec440ef6c3dc72ab22
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-09-21 01:27:48 +0530

    uc-6 played times and position-die role

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -8,36 +8,44 @@ public class SnakeLadder {
 		System.out.println("Snake & Ladder Simulator");
 		System.out.println();
 		int Singleplayer=0;
+		int count=0;
 		System.out.println("Starting position of Single Player is: "+Singleplayer);
 		
 		while(Singleplayer!=100) {
+			count++;
 			int SingleplayerRolls= random.nextInt(6)+1;
 			int a= random.nextInt(3);
 			System.out.println();
 		System.out.println("The Player rolls the die & get a number: "+SingleplayerRolls);
 		if(a==0) {
 		System.out.println("No play - stays in the same psition");
+		System.out.println("Player current position: "+Singleplayer);
 		}		
 		if(a==1) {	
 			Singleplayer += SingleplayerRolls;
 		System.out.println("Gets Ladder & Player moves ahead by number of psition: "+SingleplayerRolls);
+		System.out.println("Player current position: "+Singleplayer);
 		}
 		if(a==2) {	
 			Singleplayer -= SingleplayerRolls;
-			if(Singleplayer<=0) {
+		System.out.println("Snake bytes & Player moves Behind by number of psition: "+"-"+SingleplayerRolls);
+			if(Singleplayer<0) {
 			Singleplayer =0;
-			System.out.println("Snake bytes & Player moves Behind by number of psition: "+"-"+SingleplayerRolls);
-		}
+		System.out.println("Player current position: "+Singleplayer);
+			}
 			else
-			System.out.println("Snake bytes & Player moves Behind by number of psition: "+"-"+SingleplayerRolls);
+		System.out.println("Player current position: "+Singleplayer);
 		}
-		if(Singleplayer>100) {
+				if(Singleplayer>100) {
 			Singleplayer -= SingleplayerRolls;
 			System.out.println();
-			System.out.println(Singleplayer);
-		}
-		}
+			System.out.println("The Number goes beyond 100");
+			System.out.println("Therefore Player current position: "+Singleplayer);
+			System.out.println("The Number Required by Player is: "+(100-Singleplayer));
+	}
 		System.out.println();
-		System.out.println("Player Reaches position: "+Singleplayer);
+		System.out.println("Number of times player played: "+count);
+	}
+
 }
 }
