384c6baa917dc2fda844078470c16c8b7afc822b
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-09-20 18:01:45 +0530

    uc-2 Player Rolls Die

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.day4.UC_SnakeLadderProblem;
 
+import java.util.*;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
+		Random random = new Random();
 		System.out.println("Snake & Ladder Simulator");
 		System.out.println();
 		int Singleplayer=0;
 		System.out.println("Starting position of Single Player is: "+Singleplayer);
+		int SingleplayerRolls= random.nextInt(6)+1;
+		System.out.println("The Player rolls the die & get a number: "+SingleplayerRolls);
 	}
 
 }
